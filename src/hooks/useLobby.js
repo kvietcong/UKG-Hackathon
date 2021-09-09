6c9d524edb6017fd919c07fc27b5ad87d8aead2b
KV Le <32186663+kvietcong@users.noreply.github.com>
2021-09-09 13:56:31 -0700

    Add auto lobby add

--- src/hooks/useLobby.js
+++ src/hooks/useLobby.js
@@ -1,18 +1,32 @@
 import { db } from "../config/firebase";
 import { useState, useEffect } from "react";
-import { collection, doc, onSnapshot } from "firebase/firestore";
+import { collection, doc, onSnapshot, setDoc, getDoc } from "firebase/firestore";
 
 const useLobby = (lobbyID) => {
     const [ lobby, setLobby ] = useState({});
+    const [ exists, setExists ] = useState(false);
 
     useEffect(() => {
-        const lobbiesRef = collection(db, "lobbies")
-        const lobbyRef = doc(lobbiesRef, lobbyID)
-        const unsubscribe = onSnapshot(lobbyRef, (lobby) => {
-            setLobby(lobby.data());
-        });
-        return unsubscribe;
-    }, [ lobbyID ]);
+        const lobbiesRef = collection(db, "lobbies");
+        const lobbyRef = doc(lobbiesRef, lobbyID);
+        const seeExistAndCreate = async () => {
+            const lobbyInfo = await getDoc(lobbyRef);
+            if (!lobbyInfo.exists()) {
+                await setDoc(lobbyRef, {
+                    players: [], rounds: []
+                });
+            }
+            setExists(true);
+        }
+        if (!exists) {
+            seeExistAndCreate();
+        } else {
+            const unsubscribe = onSnapshot(lobbyRef, (lobby) => {
+                setLobby(lobby.data());
+            });
+            return unsubscribe;
+        }
+    }, [ lobbyID, exists ]);
 
     return lobby;
 };
