5c1cc8d34f1cc222c5a428444078bacab5f2584f
Andres David <andresdholguin98@gmail.com>
2021-09-09 16:56:31 -0400

    Add Lobby Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 import SomeOtherPage from "./components/SomeOtherPage";
+import Lobby from "./components/LobbyPage";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                     <Switch>
                         <Route exact path="/" component={Home}/>
                         <Route exact path="/:id" component={SomeOtherPage}/>
+                        <Route exact path="/:id/lobby" component={Lobby}/>
                     </Switch>
                 </Router>
             </ContextProvider>
