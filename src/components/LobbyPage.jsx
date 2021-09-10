c7e2a3b990be9e670d6eafb11c4b3514e4fc4d5b
KV Le <32186663+kvietcong@users.noreply.github.com>
2021-09-10 07:39:24 -0700

    Add lobby starting and copy links

--- src/components/LobbyPage.jsx
+++ src/components/LobbyPage.jsx
@@ -1,25 +1,38 @@
 import { Context } from "../Context";
-import React, { useContext, useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
 import useLobby from "../hooks/useLobby";
+import { startLobby } from "../utils/startLobby";
+import React, { useContext, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
 
-function handleStartGame(roomCode, history) {
-    history.push(`/${roomCode}/game`)
+function handleStartGame(lobbyID, history) {
+    startLobby(lobbyID);
+    history.push(`/${lobbyID}/game`)
 }
 
 const Lobby = () => {
     const { user } = useContext(Context);
-    const {lobbyID} = useParams()
-    const lobby = useLobby(lobbyID)
-    const playerList = lobby?.players ?? []
-    const history = useHistory()    
+    const { lobbyID } = useParams();
+    const lobby = useLobby(lobbyID);
+    const playerList = lobby?.players ?? [];
+    const history = useHistory();
+
+    useEffect(() => {
+        if (!lobby) return;
+        if (lobby.hasStarted) handleStartGame(lobbyID, history)
+    }, [ lobby, lobbyID, history ]);
 
     return (
         <main className="player-list">
             <h1>Lobby {lobbyID}</h1>
-            <ol>
-                {playerList.map(player => <li>{player}</li>)}
-            </ol>
+            <button className="copy" id="copyURL" onClick={() =>
+                navigator.clipboard.writeText(window.location.href)}>Copy Lobby URL
+            </button>
+            <h2>Players</h2>
+            <ul>{playerList.map(player =>
+                <li key={player} className={user === player ? "currentPlayer" : ""}>
+                    {player}
+                </li>)}
+            </ul>
             <button onClick={() => {handleStartGame(lobbyID, history)}}>Start Game</button>
         </main>
     );
